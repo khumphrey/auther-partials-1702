c0d239fcf4885cb44c09f20ac13f21e1c1b0bc60
Kate Humphrey <kate@fullstackacademy.com>
2017-04-05 11:14:56 -0400

    action 6-integration

--- server/api/auth/me.js
+++ server/api/auth/me.js
@@ -4,12 +4,12 @@ const router = require('express').Router();
 const User = require('../users/user.model');
 const HttpError = require('../../utils/HttpError');
 
+// This marries the original auth code we wrote to Passport.
+// An alternative would be to use the "local strategy" option with Passport.
  
 // check currently-authenticated user, i.e. "who am I?"
 router.get('/', function (req, res, next) {
-  User.findById(req.session.userId)
-  .then(user => res.json(user))
-  .catch(next);
+  res.json(req.user);
 });
 
 // login, i.e. "you remember `me`, right?"
@@ -22,8 +22,10 @@ router.put('/', function(req, res, next) {
       if (!user) {
         throw new HttpError(401);
       } else {
-        req.session.userId = user.id;
-        res.json(user);
+      	req.logIn(user, err => {
+      		if (err) throw new HttpError(500, err.message);
+	        res.json(user);
+      	})
       }
     })
     .catch(next);
@@ -37,18 +39,21 @@ router.post('/', function (req, res, next) {
     defaults: { password }
   })
   .spread((user, created) => {
-    if (created) {
-      req.session.userId = user.id;
-      res.json(user);
-    } else {
+    if (!created) {
       throw new HttpError(401);
+    } else {
+      req.logIn(user, err => {
+      		if (err) throw new HttpError(500, err.message);
+	        res.json(user);
+      	})
     }
   });
 });
 
 // logout, i.e. "please just forget `me`"
 router.delete('/', function (req, res, next) {
-	req.session.destroy(); // destroys entire session
+	req.logOut();
+	// req.session.destroy(); // destroys entire session
   // delete req.session.userId; // deletes one item on session
   res.sendStatus(204);
 });
