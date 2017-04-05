0e87a9e4c543fe18f8f20a4fef46edb46564033a
Kate Humphrey <kate@fullstackacademy.com>
2017-04-05 10:30:07 -0400

    1-counter

--- server/app/index.js
+++ server/app/index.js
@@ -1,7 +1,9 @@
 'use strict';
+require('../../secrets');
 
-var app = require('express')();
-var path = require('path');
+const app = require('express')();
+const path = require('path');
+const session = require('express-session');
 
 // "Enhancing" middleware (does not send response, server-side effects only)
 
@@ -9,13 +11,24 @@ app.use(require('./logging.middleware'));
 
 app.use(require('./body-parsing.middleware'));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use('/api', (req, res, next) => {
+  if (!req.session.counter) req.session.counter = 0;
+  console.log('counter',++req.session.counter);
+  next();
+});
 
 // "Responding" middleware (may send a response back to client)
 
 app.use('/api', require('../api/api.router'));
 
-var validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
-var indexPath = path.join(__dirname, '..', '..', 'browser', 'index.html');
+const validFrontendRoutes = ['/', '/stories', '/users', '/stories/:id', '/users/:id', '/signup', '/login'];
+const indexPath = path.join(__dirname, '..', '..', 'browser', 'index.html');
 validFrontendRoutes.forEach(function (stateRoute) {
   app.get(stateRoute, function (req, res) {
     res.sendFile(indexPath);
